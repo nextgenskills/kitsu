8bb63345436232a54a590fee2651e53377c8ced1
Tom Box <tom@blue-zoo.co.uk>
2023-05-14 21:33:54 +0100

    Added help text settings

---
+++ src/lib/helptext.js
@@ -0,0 +1,5 @@
+
+export const HELP_TEXT = [ 
+ {section:'default',values:   ["We'll show you lots of hints and tips on how you can perfect your professional skills within each area of the studio pipeline and everyday interactions with colleagues."]
+}
+]
